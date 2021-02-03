59097a214b46e9d50119297bb0c1c05cedc1726c
yidihenkeng <37501572+yidihenkeng@users.noreply.github.com>
2021-02-03 18:16:01 +0800

    Update CSNet_ResNet50.bat

--- network/CSNet_ResNet50.bat
+++ network/CSNet_ResNet50.bat
@@ -1,4 +1,4 @@
-E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/likaiwen/network_LIVEMD/network3_4_1/CSNet_ResNet50_solver.prototxt --gpu 3 --weights=D:/likaiwen/network_RES_TID2013/ResNet-50-model.caffemodel 
+E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/123/network_LIVEMD/network3_4_1/CSNet_ResNet50_solver.prototxt --gpu 3 --weights=D:/123/network_RES_TID2013/ResNet-50-model.caffemodel 
 
 pause
 
