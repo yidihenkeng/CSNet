4346ca6811323a62b7e108201dfbba8151db4b71
yidihenkeng <37501572+yidihenkeng@users.noreply.github.com>
2021-02-03 18:18:28 +0800

    Update CSNet_combined_feature.bat

--- network/CSNet_combined_feature.bat
+++ network/CSNet_combined_feature.bat
@@ -1,4 +1,4 @@
-E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/likaiwen/network_LIVE/network3_4_2/CSNet_combined_feature_solver.prototxt --gpu 1
+E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/123/network_LIVE/network3_4_2/CSNet_combined_feature_solver.prototxt --gpu 1
 
 pause
 
