7c14827ec4f78c9ce1443dcd517d3d1f87c28eea
yidihenkeng <37501572+yidihenkeng@users.noreply.github.com>
2021-02-03 18:15:14 +0800

    Update CSNet_VGG16.bat

--- network/CSNet_VGG16.bat
+++ network/CSNet_VGG16.bat
@@ -1,4 +1,4 @@
-E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/likaiwen/network_LIVEMD/network3_4_1/CSNet_VGG16_solver.prototxt --gpu 0 --weights=D:/likaiwen/network_LIVE/network3_4_VGG16/VGG_ILSVRC_16_layers.caffemodel 
+E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/123/network_LIVEMD/network3_4_1/CSNet_VGG16_solver.prototxt --gpu 0 --weights=D:/123/network_LIVE/network3_4_VGG16/VGG_ILSVRC_16_layers.caffemodel 
 
 pause
 
