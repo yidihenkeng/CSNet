39d142aae2b6348628909acab85ec2bfb5150428
yidihenkeng <37501572+yidihenkeng@users.noreply.github.com>
2021-02-03 18:20:21 +0800

    Update CSNet_yiyuan.bat

--- network/CSNet_yiyuan.bat
+++ network/CSNet_yiyuan.bat
@@ -1,4 +1,4 @@
-E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/likaiwen/network_LIVE/network3_4_3/CSNet_zhixin_solver.prototxt  --gpu 1
+E:/Projects/caffe/tools/Release/caffe.exe train --solver=D:/123/network_LIVE/network3_4_3/CSNet_zhixin_solver.prototxt  --gpu 1
 
 pause
 
